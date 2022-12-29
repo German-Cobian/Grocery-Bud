a8ab89f89d941d8f9d18991e11a3e8b667c886c1
German Renato Cobian <germancobian@hotmail.com>
2022-12-29 14:33:49 -0800

    Declare the various useState hooks + display Alert and List components from App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import './App.css';
+import './App.css'
+import React, { useState } from 'react';
 import List from './List';
 import Alert from './Alert';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>This is a React app</h1>
-    </div>
-  );
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editD, setEditID] = useState(null);
+  const [alert, setAlert] = useState({show: false, mg: '', type: ''});
+
+  return <section className="section-center">
+      <div className='grocery-container'>
+        <Alert />
+        <List />
+      </div>
+    </section>
 }
 
 export default App;
