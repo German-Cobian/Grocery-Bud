01314a17659f2d5c4d409a1417b8243f8a0bd31c
German Renato Cobian <germancobian@hotmail.com>
2022-12-30 13:11:47 -0800

    Tie Alert component to handleSubmit

--- src/App.js
+++ src/App.js
@@ -8,13 +8,19 @@ function App() {
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
-  const [alert, setAlert] = useState({show: false, mg: '', type: ''});
+  const [alert, setAlert] = useState({show: true, mg: '', type: ''});
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('hello')
+  }
 
   return <section className="section-center">
       <div className="grocery-container">
-        <Alert />
+        <form className="grocery-form" onSubmit= {handleSubmit}>
+          {alert.show && <Alert />}
+        </form>
         <List />
-        <button className="clear-btn"></button>
+        <button className="clear-btn">Clear Items</button>
       </div>
     </section>
 }
